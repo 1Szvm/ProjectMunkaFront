82bc87676e5855f450ed1a47757dc490214ab8de
1Szvm <szilajmark2005@gmail.com>
2025-01-22 15:43:15 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Header } from './components/Header'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+const router=createBrowserRouter([
+  {element:<Header/>,
+    children:[
+      {path:'/',element:<Home />},
+      /*{path:'/posts',element:<Posts />},
+      {path:'/detail/:id',element:<Detail />},
+      {path:'/create',element:<AddEditPost/>},
+      {path:'/update/:id',element:<AddEditPost/>},
+      {path:'/auth/in',element:<Auth />},
+      {path:'/auth/up',element:<Auth />},
+      {path:'/pwreset',element:<PwReset />},
+      {path:'/profile',element:<Profile />},
+      {path:'/admin',element:<Admin />},  */
+      {path:'*',element:<NotFound />},
+    ]
+  }
+],
+{
+  future: {
+    v7_relativeSplatPath: true,
+    v7_normalizeFormMethod: true,
+    v7_fetcherPersist: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  }} 
+)
+
+function App() {
+  return <RouterProvider router={router}   future={{v7_startTransition: true}}/>
+}
+
+export default App
