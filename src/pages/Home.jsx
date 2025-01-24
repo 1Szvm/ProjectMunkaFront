be6f27d6bacc71029520409e47df16b5436a2494
Roland141 <rolika141@gmail.com>
2025-01-24 11:40:00 +0100

    HotFix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
     <>
     <div className='justify-center items-center flex m-0 w-full'>
       <div className='overflow-hidden'>
-           <video autoPlay loop muted className='w-full'>
+           <video autoPlay loop muted className=''>
               <source src='Project3.mp4' type="video/mp4" className='w-full'/>
            </video>
       </div>
