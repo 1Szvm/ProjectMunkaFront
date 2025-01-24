5fdca737a81bbacc028f0eb8f3d66cb576b32e55
Roland141 <rolika141@gmail.com>
2025-01-24 11:16:49 +0100

    Home

--- src/pages/Futamok.jsx
+++ src/pages/Futamok.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 
 
+
 export const Futamok = () => {
   return (
     <>
-    <div className='justify-center items-center h-screen '>
-      <div className=''>
-           <video autoPlay loop muted plays-inline class="background-clip" className='absolute right-0 bottom-0 -z-10' >
-              <source src='Background-clip.mov' type="video/mp4"/>
-           </video>
-        </div>
+
+
 
         
-    </div>
+    
     </>
   )
 }
