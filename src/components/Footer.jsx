01fb6703381fa4549c4a70949489e8c1a96aefd1
wolfkinghun <viovica1984@gmail.com>
2025-01-31 11:39:19 +0100

    BaseFooter_and_home_rework_to_phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-      <footer style={{ textAlign: "center", padding: "10px", background: "#333", color: "#fff" }}>
+      <footer style={{ height:'120px', textAlign: "center",justifyContent:'center', padding: "10px", background: "#333", color: "#fff" }}>
         <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
       </footer>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
 
 export const Home = () => {
-  console.log('Home');
+  
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 1068px)").matches
+  );
+  window.onresize = function(event)
+{
+document.location.reload(true);
+}
+
+  // Update match state when the screen size changes
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1068px)");
+    const handleMediaQueryChange = (e) => setMatches(e.matches);
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
   
   return (
     <>
-    <div className='justify-center items-center flex m-0 w-full'>
-      <div className='overflow-hidden'>
-           <video autoPlay loop muted className=''>
-              <source src='Project3.mp4' type="video/mp4" className='w-full'/>
-           </video>
-      </div>
-      <div>
-      
-      </div>
-    </div>
+      {matches ? (
+        <div className='justify-center items-center flex m-0 w-full'>
+          <div className='overflow-hidden'>
+            <video autoPlay loop muted className=''>
+              <source src='Project3.mp4' type='video/mp4' className='w-full'/>
+            </video>
+          </div>
+          <div></div>
+        </div>
+      ) : (
+        <div className='justify-center items-center flex m-0 w-full'>
+          <div className='overflow-hidden'>
+            <video autoPlay loop muted className=''>
+              <source src='IAMBACK.mp4' type='video/mp4' className='w-full h-full'/>
+            </video>
+          </div>
+          <div></div>
+        </div>
+      )}
     </>
-  )
-}
-
-
+  );
+};
