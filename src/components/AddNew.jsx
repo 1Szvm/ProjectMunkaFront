e6deea92c9fe01317e59c0df76535124297bd9f6
wolfkinghun <viovica1984@gmail.com>
2025-03-13 11:56:11 +0100

    klme

--- src/components/AddNew.jsx
+++ src/components/AddNew.jsx
@@ -1,122 +1,100 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import { addFutam, readAuthorization, readCategories, updatePost } from '../utility/crudUtility';
+import { addFutam, readAuthorization, readCategories  } from '../utility/crudUtility';
 import { useForm } from 'react-hook-form';
 import { UserContext } from '../context/UserContext';
 import { useParams } from 'react-router-dom';
 import { uploadFile } from '../utility/uploadFile';
 import Alerts from './Alerts';
 
-export default function AddNew({ addEdit, setAddEdit }) {
+export default function AddNew({addEdit, setAddEdit}) {
     const { user } = useContext(UserContext);
     const { register, handleSubmit, formState: { errors }, reset, setValue } = useForm();
     const [admins, setAdmins] = useState(null);
     const params = useParams();
-    const [categories, setCategories] = useState(null);
-    const [txt, setText] = useState(null);
-    const [err, setErr] = useState(null);
+    const [categories, setCategories] = useState(null);    
+    const [txt,setText]=useState(null)
+    const [erro, setErr] = useState(null);
     const modalRef = useRef(null);
     const [isEditMode, setIsEditMode] = useState(false);
 
     const [idopont, setIdopont] = useState("");
     const [selectedCateg, setSelectedCateg] = useState("");
-    const [palya, setPalya] = useState("");
-    const [max, setMax] = useState(1);
-    const [photo, setPhoto] = useState("");
+    const [palya,setPalya]=useState(null)
+    const [max,setMax]=useState(null)
+    const [photo, setPhoto] = useState(null);
 
     useEffect(() => {
         readCategories(setCategories);
         readAuthorization(setAdmins);
     }, []);
 
+    //This looks awful, but it's working I guess
     useEffect(() => {
         if (addEdit === false) {
-            modalRef.current?.close();
-            document.getElementById("file").disabled = false;
-            reset();
-            setPhoto(null);
-            setSelectedCateg("");
-            setPalya("");
-            setMax(1);
-            setIdopont("");
-        } else if (addEdit === true) {
+            console.log("false");
             setIsEditMode(false);
-            reset();
+            document.getElementById("file").disabled = false;
+            modalRef.current?.close();
+        } 
+        else if (addEdit === true) {
+            console.log("true");
+            setIsEditMode(false);
             modalRef.current?.showModal();
-        } else if (typeof addEdit === "object") {
-            setSelectedCateg(addEdit?.kategoria);
-            setPalya(addEdit?.palya);
-            setMax(addEdit?.max);
-            setIdopont(new Date(addEdit?.idopont.seconds * 1000).toISOString().split('T')[0]);
+        } 
+        else if (typeof addEdit === "object") {
+            console.log("edit mode");
             setIsEditMode(true);
             document.getElementById("file").disabled = true;
             modalRef.current?.showModal();
-
-            setValue('track', addEdit?.palya);
-            setValue('maxracers', addEdit?.max);
-            setValue('date', new Date(addEdit?.idopont.seconds * 1000).toISOString().split('T')[0]);
+            
+            setSelectedCateg(addEdit?.kategoria);
+            document.getElementById("track").value = addEdit?.palya;
+            document.getElementById("maxracers").value = addEdit?.max;
+            document.getElementById("date").value = new Date(addEdit?.idopont.seconds * 1000)
+                .toISOString()
+                .split('T')[0];
         }
-    }, [addEdit, reset, setValue]);
+    }, [addEdit]);
+    
 
-
+    //WHY IS THIS SHIT NOT FUCKING WORKING THIS IS UTTERLY FUCKING RETARDED!!!!!! (I cant belive this shit it was a fucking typo all along)
     const onSubmit = async (data) => {
-        if(isEditMode){
-            try {
-                await updatePost(addEdit.id, {
-                    ...data,
-                    idopont: new Date(data.date),
-                    kategoria: data.category,  
-                    max: data.maxracers,
-                    palya: data.track
-                });
-        
-                setText("Sikeresen mentés!");
-                setTimeout(() => modalRef.current?.close(), 800);
-            } catch (error) {
-                console.log("update error: ", error);
-                setErr("Sikertelen");
-            } finally {
-                setAddEdit(false);
+        try {
+            let newRaceData = {
+                ...data,
+                idopont:new Date(idopont),
+                kategoria: selectedCateg,
+                max,
+                palya,
+                resztvevok: [],
+            };
+            console.log(newRaceData);
+            let newPostData = { ...newRaceData };
+            const file = data.file?.[0];
+            if (file) {
+                const { url, id } = await uploadFile(file);
+                newPostData.imageUrl = { url, id };
             }
-        }else{
-            try {
-                let newRaceData = {
-                    ...data,
-                    idopont: new Date(idopont),
-                    kategoria: selectedCateg,
-                    max,
-                    palya,
-                    resztvevok: [],
-                };
-                let newPostData = { ...newRaceData };
-                const file = data.file?.[0];
-                if (file) {
-                    const { url, id } = await uploadFile(file);
-                    newPostData.imageUrl = { url, id };
-                }
-                await addFutam(newPostData);
-                reset({ date: "", maxracers: "", track: "" });
-                setPhoto(null);
-                setText("Sikeresen hozzáadva!");
-            } catch (error) {
-                console.error("Error submitting form:", error);
-                setErr("Sikertelen");
-            } finally {
-                setTimeout(() => modalRef.current?.close(), 800);
-            }
+            await addFutam(newPostData);
+            reset({ date: "", maxracers: "", track: "" });
+            setPhoto(null);
+            setText("Sikeresen hozzáadva!")
+        }
+        catch (error) {
+            console.error("Error submitting form:", error);
+            setErr("Sikertelen")
+        } finally {
+            setTimeout(() => modalRef.current?.close(), 800);
         }
     };
+    
 
     return (
         <div>
             {admins?.some(admin => admin.Ids.includes(user?.uid)) && (
                 <div
-<<<<<<< HEAD
                     className="fixed bottom-5 right-5 flex justify-center items-center w-16 h-16 rounded-full shadow-lg cursor-pointer transition-transform duration-300 bg-rose-600"
                     onClick={()=>setAddEdit(true)}
-=======
-                    className="fixed bottom-5 right-5 flex justify-center items-center w-16 h-16 rounded-full shadow-lg cursor-pointer transition-transform duration-300 bg-red-600"
-                    onClick={() => setAddEdit(true)}
->>>>>>> 37236354b444207815e85b19fb54ae764a3655ce
                 >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -130,7 +108,6 @@ export default function AddNew({ addEdit, setAddEdit }) {
                     </svg>
                 </div>
             )}
-<<<<<<< HEAD
 
             <dialog ref={modalRef} id="add" className="modal">
                 <div className="modal-box max-h-screen p-6 rounded-2xl shadow-lg bg-emerald-600">
@@ -161,7 +138,7 @@ export default function AddNew({ addEdit, setAddEdit }) {
                         <p className="text-red-600">{errors?.category?.message}</p>
 
 
-                        <label className="block  font-medium mt-4">Pálya neve</label>
+                        <label className="block font-medium mt-4">Pálya neve</label>
                         <input id="track" type="text" placeholder="Pálya neve"
                             className="input text-black w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             {...register('track', { required: 'A név megadása kötelező.' })}
@@ -174,7 +151,7 @@ export default function AddNew({ addEdit, setAddEdit }) {
                             disabled={params.id}
                             id="file"
                             type="file"
-                            className="file-input text-black w-full border border-gray-400 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="file-input text-black w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             {...register("file", params.id ? {} : {
                                 required: !params.id,
                                 validate: (value) => {
@@ -196,7 +173,7 @@ export default function AddNew({ addEdit, setAddEdit }) {
                             id="maxracers" 
                             type="number" 
                             min="1" max="100"
-                            className="input text-black  w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="input w-full border text-black border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             {...register("maxracers", { 
                                 required: "Minimum 1 versenyző szükséges!", 
                                 min: { value: 1, message: "Minimum 1 versenyző szükséges!" } 
@@ -207,87 +184,13 @@ export default function AddNew({ addEdit, setAddEdit }) {
 
                         <label className="block font-medium mt-4">Dátum kiválasztása</label>
                         <input id="date" type="date"
-                            className="text-black input w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="input w-full text-black border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             {...register('date', { required: 'A dátum megadása kötelező.' })}
                             onChange={(e) => setIdopont(e.target.value)
-=======
-        <dialog ref={modalRef} id="add" className="modal">
-            <div className="modal-box max-h-screen p-6 rounded-2xl shadow-lg">
-                <h3 className="font-bold text-xl text-center mb-4">Verseny létrehozása</h3>
-                <div>
-                    <label 
-                        className="block font-medium mb-2"
-                        htmlFor="category"
-                    >
-                        Játék kiválasztása
-                    </label>
-                    <select
-                        id="category"
-                        className="select w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        value={selectedCateg}
-                        onClick={(e) => {
-                            setSelectedCateg(e.target.value);
-                        }}
-                        {...register('category', { required: 'A kategória kiválasztása kötelező.' })}
-                    >
-                        <option value="" disabled>Válaszd ki a játékot</option>
-                        {categories && categories.map(obj => (
-                            <option key={obj.id} value={obj.id} style={{ color: obj.color }}>
-                                {obj.nev}
-                            </option>
-                        ))}
-                    </select>
-                    <p className="text-red-600">{errors?.category?.message}</p>
-
-
-                    <label className="block font-medium mt-4">Pálya neve</label>
-                    <input id="track" type="text" placeholder="Pálya neve"
-                        className="input w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        {...register('track', { required: 'A név megadása kötelező.' })}
-                        onChange={(e) => setPalya(e.target.value)}
-                        value={palya}
-                    />
-                    <p className="text-red-600">{errors?.track?.message}</p>
-
-                    <label className="block font-medium mt-4">Pálya kép</label>
-                    <input
-                        disabled={params.id}
-                        id="file"
-                        type="file"
-                        className="file-input w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        {...register("file", params.id ? {} : {
-                            required: !params.id,
-                            validate: (value) => {
-                                if (!value[0]) return true;
-                                const fileExtension = value[0]?.name.split(".").pop().toLowerCase();
-                                const acceptedFormats = ['jpg', 'png'];
-                                if (!acceptedFormats.includes(fileExtension)) return "Invalid fájl formátum!";
-                                if (value[0].size > 1 * 1000 * 1024) return "Az engedélyezett fájl mérete 1MB";
-                                return true;
->>>>>>> 37236354b444207815e85b19fb54ae764a3655ce
                             }
-                        })}
-                        onChange={(e) => setPhoto(URL.createObjectURL(e.target.files[0]))}
-                    />
-                    {photo && <img src={photo} alt="Preview" className="img-thumbnail mt-3" />}
-                    <p className="text-red-600">{errors?.file?.message}</p>
-                    
-                    <label className='block font-medium mt-4'>Versenyzők száma</label>
-                    <input 
-                        id="maxracers" 
-                        type="number" 
-                        min="1" max="100"
-                        className="input w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        {...register("maxracers", { 
-                            required: "Minimum 1 versenyző szükséges!", 
-                            min: { value: 1, message: "Minimum 1 versenyző szükséges!" } 
-                        })}
-                        onChange={(e) => setMax(e.target.value)}
-                        value={max}
-                    />
-                    {errors.maxracers && <p className="text-red-500 text-sm mt-1">{errors.maxracers.message}</p>}
+                        />
+                        <p className="text-red-600">{errors?.date?.message}</p>
 
-<<<<<<< HEAD
                         <div className="modal-action flex justify-between mt-6">
                             {!isEditMode && (
                                 <div 
@@ -313,38 +216,11 @@ export default function AddNew({ addEdit, setAddEdit }) {
                             >
                                 Bezárás
                             </div>
-=======
-                    <label className="block font-medium mt-4">Dátum kiválasztása</label>
-                    <input id="date" type="date"
-                        className="input w-full border border-gray-300 rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        {...register('date', { required: 'A dátum megadása kötelező.' })}
-                        onChange={(e) => setIdopont(e.target.value)
-                        }
-                        value={idopont}
-                    />
-                    <p className="text-red-600">{errors?.date?.message}</p>
-
-                    <div className="modal-action flex justify-between mt-6">
-                        <div 
-                            id='addBtn'
-                            className={`btn text-white px-4 py-2 rounded-lg cursor-pointer ${isEditMode?"bg-blue-600 hover:bg-blue-700":"bg-green-600 hover:bg-green-700"}`}
-                            onClick={()=>handleSubmit(onSubmit)()}
-                        >
-                            {isEditMode?"Szerkesztés mentése":"Létrehozás"}
-                        </div>
-                        <div 
-                            className="btn text-white bg-red-500 hover:bg-red-600 px-4 py-2 rounded-lg cursor-pointer"
-                            onClick={() => setAddEdit(false)}
-                        >
-                            Bezárás
->>>>>>> 37236354b444207815e85b19fb54ae764a3655ce
                         </div>
                     </div>
                 </div>
-            </div>
-        </dialog>
-        {txt&&<Alerts txt={txt}/>}
-        {err&&<Alerts err={err}/>}
-    </div>
+            </dialog>
+            {txt&&<Alerts txt={txt}/>}
+        </div>
     );
 }
