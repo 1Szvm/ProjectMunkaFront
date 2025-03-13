5efbd8e2065e41c770718d138fb52e2dc5d115d6
wolfkinghun <viovica1984@gmail.com>
2025-03-13 12:48:36 +0100

    fx2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -148,6 +148,7 @@ export const Header = () => {
           <motion.button
             className="md:hidden btn btn-ghost text-xl"
             onClick={toggleMobileMenu}
+            onBlur={() => setIsMobileMenuOpen(false)}
             whileHover={{ scale: 1.1 }}
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
