f2c3a9878bf58ae7cbf4c96a493a9e490e8c8ee2
1Szvm <szilajmark2005@gmail.com>
2025-03-19 20:26:25 +0100

    fixes

--- src/pages/ForumPost.jsx
+++ src/pages/ForumPost.jsx
@@ -72,32 +72,40 @@ export default function ForumPost() {
             {post ? (
                 <div className='py-4'>
                   {/*Post*/}
-                  <div className='flex items-start'>
-                    {/*small when big text*/}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 mt-2 cursor-pointer opacity-90 hover:opacity-100" onClick={()=>navigate("/forum")}>
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-                    </svg>
-                    <div className='mx-2'>
-                      <div>
-                        <h1 className="text-4xl font-bold">{post.title}</h1>
-                        {user?.uid === post.uid 
-                          ? <div className="text-lg">Te</div>
-                          : <div className="text-lg">
-                              {users?.find(obj => obj.uid === post.uid)?.displayName || 
-                                <div className='flex'>
-                                  <span className="text-red-600">törölt felhasználó</span>
-                                  <div className='text-xs m-1'>({post.uid})</div>
-                                </div>
-                              }
-                            </div>
-                        }
-                        {(admins?.some(admin => admin.Ids.includes(user?.uid)) || (user?.uid === commentsArray[0]))? (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-2 cursor-pointer size-6" onClick={()=>handleDeletePost(param.id)}>
+                  <div>
+                    <div className='flex justify-between mb-2'>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+                        className="size-8 mb-5 cursor-pointer opacity-90 hover:opacity-100" 
+                        onClick={()=>navigate("/forum")}
+                        >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                      </svg>
+                      {(admins?.some(admin => admin.Ids.includes(user?.uid)) || (user?.uid === post.uid[0]))? (
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="cursor-pointer size-6" onClick={()=>handleDeletePost(param.id)}>
                           <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>)
                         :
                         null}
-                        <div>{new Date(post.letrehozas.toDate()).toLocaleDateString()}</div>
+                    </div>
+                    <div className='mx-2'>
+                      <div className='flex justify-between'>
+                        <div>
+                          <h1 className="text-4xl font-bold">{post.title}</h1>
+                          {user?.uid === post.uid 
+                            ? <div className="text-lg">Te</div>
+                            : <div className="text-lg">
+                                {users?.find(obj => obj.uid === post.uid)?.displayName || 
+                                <>
+                                  <div className="text-red-600">törölt felhasználó</div>
+                                  <div className='text-xs m-1'>({post.uid})</div>
+                                </>
+                                }
+                              </div>
+                          }
+                        </div>
+                        <div>
+                          <div>{new Date(post.letrehozas.toDate()).toLocaleDateString()}</div>
+                        </div>
                       </div>
                       <div className="my-5 p-2 text-lg">
                         {post.content}
@@ -122,7 +130,7 @@ export default function ForumPost() {
                   </div>
                   <div>
                     {/*comment section*/}
-                    {Object.entries(post.comments || {}).map(([commentId, commentsArray]) => (
+                    {Object.entries(post?.comments || {}).map(([commentId, commentsArray]) => (
                       <div key={commentId} className='m-2 bg-slate-800 rounded-xl p-3'>
                         <div className='flex justify-between'>
                           <div>
@@ -130,10 +138,10 @@ export default function ForumPost() {
                               ? <div className="text-lg">Te</div>
                               : <div className="text-lg">
                                   {users?.find(obj => obj.uid === commentsArray[0])?.displayName || 
-                                    <div className='flex'>
-                                      <span className="text-red-600">törölt felhasználó</span>
-                                      <div className='text-xs m-1'>({commentsArray[0]})</div>
-                                    </div>
+                                  <>
+                                    <div className="text-red-600">törölt felhasználó</div>
+                                    <div className='text-xs m-1'>({commentsArray[0]})</div>
+                                  </>
                                   }
                                 </div>
                             }
